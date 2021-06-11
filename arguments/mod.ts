03f2b9c87055836505b2fd2127b3691817a5fd80
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-11 14:43:52 +0200

    feat(arguments): More info about arguments

--- arguments/mod.ts
+++ arguments/mod.ts
@@ -1,18 +1,18 @@
-import { NaticoClient, NaticoCommandHandler } from '../deps.ts';
+import { NaticoClient, NaticoCommandHandler } from "../deps.ts";
 class BotClient extends NaticoClient {
-	constructor() {
-		super({});
-	}
-	commandHandler: NaticoCommandHandler = new NaticoCommandHandler(this, {
-		directory: './commands',
-		prefix: '!',
-		owners: ['336465356304678913'],
-		guildonly: false,
-	});
-	async start(token: string) {
-		await this.commandHandler.loadALL();
-		return this.login(token);
-	}
+  constructor() {
+    super({});
+  }
+  commandHandler: NaticoCommandHandler = new NaticoCommandHandler(this, {
+    directory: "./commands",
+    prefix: "!",
+    owners: ["336465356304678913"],
+    guildonly: false,
+  });
+  async start(token: string) {
+    await this.commandHandler.loadALL();
+    return this.login(token);
+  }
 }
 const botClient = new BotClient();
 botClient.start(token);
--- deps.ts
+++ deps.ts
@@ -1,2 +1,1 @@
-export * from 'https://deno.land/x/natico@1.2.1/mod.ts';
-export * from 'https://deno.land/x/discordeno@11.0.3/mod.ts';
+export * from "https://deno.land/x/natico@1.2.3/mod.ts";
