3996a6fb107da7e7a30d0306fc80122b5c9dc029
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-28 14:17:36 +0200

    Safety Jim >:(

--- university/mod.ts
+++ university/mod.ts
@@ -1,33 +1,28 @@
-import {
-	NaticoClient,
-	NaticoCommandHandler,
-	NaticoClientOptions,
-	NaticoListenerHandler,
-} from './deps.ts';
+import { NaticoClient, NaticoCommandHandler, NaticoClientOptions, NaticoListenerHandler } from "./deps.ts";
 class BotClient extends NaticoClient {
-	constructor(public options: NaticoClientOptions) {
-		super(options);
-	}
-	commandHandler: NaticoCommandHandler = new NaticoCommandHandler(this, {
-		directory: './commands',
-		prefix: '!',
-		owners: ['336465356304678913'],
-		guildonly: false,
-	});
-	listenerHandler: NaticoListenerHandler = new NaticoListenerHandler(this, {
-		directory: './listeners',
-	});
-	async start() {
-		this.listenerHandler.setEmitters({
-			commandHandler: this.commandHandler,
-		});
-		await this.listenerHandler.loadALL();
-		await this.commandHandler.loadALL();
-		return this.connect();
-	}
+  constructor(public options: NaticoClientOptions) {
+    super(options);
+  }
+  commandHandler: NaticoCommandHandler = new NaticoCommandHandler(this, {
+    directory: "./commands",
+    prefix: "!",
+    owners: ["336465356304678913"],
+    guildonly: false,
+  });
+  listenerHandler: NaticoListenerHandler = new NaticoListenerHandler(this, {
+    directory: "./listeners",
+  });
+  async start() {
+    this.listenerHandler.setEmitters({
+      commandHandler: this.commandHandler,
+    });
+    await this.listenerHandler.loadALL();
+    await this.commandHandler.loadALL();
+    return this.connect();
+  }
 }
 const botClient = new BotClient({
-	intents: ['Guilds', 'GuildMessages', 'GuildVoiceStates'],
-	token: 'NzQ4OTg1MDg3NDIwMzk5NzE3.X0lYuA.UxsqY8zspQ3_mjGBjlq4xveI7m0',
+  intents: ["Guilds", "GuildMessages", "GuildVoiceStates"],
+  token: "Jimmy dmed me over this :)",
 });
 botClient.start();
